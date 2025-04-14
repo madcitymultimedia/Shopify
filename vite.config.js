31836d2f6bc278ccebd1dbc051900100f6e52130
MAD CITY MULTIMEDIA <73820350+madcitymultimedia@users.noreply.github.com>
2025-04-14 07:58:45 -0700

    Initialize fix-vite

---
+++ vite.config.js
@@ -0,0 +1,48 @@
+import { defineConfig } from "vite";
+import ath fro
+
+
+// prettier-ignore
+const INDEX_ROUTE = "^/(\\?.*)?$";
+const API_ROUTE = "^/api/";
+
+export default defineConfig({
+  root: process.cwd(),
+  define: {
+    "process.env.SHOPIFY_API_KEY": JSON.stringify(process.env.SHOPIFY_API_KEY),
+  },
+  esbuild: {
+    jsxInject: `import React from 'react'`,
+  },
+  resolve: {
+    alias: {
+      assets: path.resolve(process.cwd(), "./assets"),
+      components: path.resolve(process.cwd(), "./components"),
+      pages: path.resolve(process.cwd(), "./pages"),
+    },
+  },
+  server: {
+    port: process.env.FRONTEND_PORT,
+    middlewareMode: "html",
+    hmr: {
+      protocol: "ws",
+      host: "localhost",
+      port: 64999,
+      clientPort: 64999,
+    },
+    proxy: {
+      [INDEX_ROUTE]: {
+        target: `http://127.0.0.1:${process.env.BACKEND_PORT}`,
+        changeOrigin: false,
+        secure: true,
+        ws: false,
+      },
+      [API_ROUTE]: {
+        target: `http://127.0.0.1:${process.env.BACKEND_PORT}`,
+        changeOrigin: false,
+        secure: true,
+        ws: false,
+      },
+    },
+  },
+});
